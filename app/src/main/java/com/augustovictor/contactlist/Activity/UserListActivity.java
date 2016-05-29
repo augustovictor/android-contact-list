a6795c4b2d074b3de63b7f653c5fdb919d306b46
Victor <victoraweb@gmail.com>
2016-05-28 22:57:18 -0300

    Add contacts button functionality.

--- app/src/main/java/com/augustovictor/contactlist/Activity/UserListActivity.java
+++ app/src/main/java/com/augustovictor/contactlist/Activity/UserListActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.augustovictor.contactlist.Adapter.ContactsAdapter;
 import com.augustovictor.contactlist.Model.Contact;
@@ -19,17 +21,20 @@ public class UserListActivity extends AppCompatActivity {
 
     // STEP 1
     ArrayList<Contact> contacts;
+    Button mAddContactsButton;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         RecyclerView rvContacts = (RecyclerView) findViewById(R.id.recycler_view);
 
         contacts = Contact.createContactList(20);
 
-        ContactsAdapter adapter = new ContactsAdapter(contacts);
+        final ContactsAdapter adapter = new ContactsAdapter(contacts);
 
         rvContacts.setAdapter(adapter);
 
@@ -37,5 +42,15 @@ public class UserListActivity extends AppCompatActivity {
         mLinearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
 
         rvContacts.setLayoutManager(mLinearLayoutManager);
+
+        mAddContactsButton = (Button) findViewById(R.id.add_contacts_button);
+
+        mAddContactsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contacts.add(contacts.size(), new Contact("Victor", true));
+                adapter.notifyItemInserted(contacts.size());
+            }
+        });
     }
 }
